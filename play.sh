fd840ede3730da6b06ee73675605eda23737d65e
Cédric Girard <c.girard@epiconcept.fr>
2018-05-25 10:12:39 +0200

    hold sur keepassx

--- play.sh
+++ play.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ansible-playbook run.yml --ask-become-pass $@
+inventory=hosts
+playbook=run.yml
+
+if [ "$1" == "help" ]; then
+	ansible-playbook -vvvv -i $inventory --list-hosts $playbook
+	ansible-playbook -i $inventory --list-tags $playbook
+else
+	ansible-playbook -i $inventory --ask-become-pass $playbook $@
+fi
