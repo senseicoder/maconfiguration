fbb73035840ff3ae93f981a32a83e27545476df2
Cédric Girard <c.girard@epiconcept.fr>
2020-01-28 11:08:04 +0100

    mise au point ocs

--- roles/ocsinventory-install/files/ocsinventory_runif.sh
+++ roles/ocsinventory-install/files/ocsinventory_runif.sh
@@ -3,5 +3,5 @@
 
 curl -s http://$(grep ^server /etc/ocsinventory/ocsinventory-agent.cfg | sed -e 's/.*=//') > /dev/null
 if [ $? -eq 0 ]; then
-	ocsinventory-agent
+	ocsinventory-agent 2>&1 | grep -v '^\[info\]' | mail -Es "$(hostname) ocsinventory-agent" -a "From: cedric@daneel.net" c.girard@epiconcept.fr
 fi
